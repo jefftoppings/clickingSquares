e2f4b015baea1c43cd4c34a7a87c0df081786f7d
Jeffrey Toppings <jtt607@mail.usask.ca>
2019-04-12 14:30:14 -0600

    Added event handler for start button

--- app/src/main/java/com/example/clickingsquares/MainMenu.java
+++ app/src/main/java/com/example/clickingsquares/MainMenu.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Space;
@@ -20,21 +21,27 @@ public class MainMenu extends LinearLayout {
         setLayoutParams(new LayoutParams(MainActivity.width, MainActivity.height));
         setOrientation(VERTICAL);
 
+        // container
+        LinearLayout container = new LinearLayout(context);
+        container.setOrientation(VERTICAL);
+        addView(container);
+
         // title of Main Menu
         title = new TextView(context);
-        title.setText("\nCLICKING\nSQUARES\n");
+        title.setText("CLICKING\nSQUARES\n");
         title.setTextAlignment(TEXT_ALIGNMENT_CENTER);
         title.setTextSize(50);
         title.setWidth(MainActivity.width);
-        addView(title);
+        container.addView(title);
 
         // Add a start button
         startButton = new Button(context);
         startButton.setText("Start");
         startButton.setTextSize(30);
-        startButton.setBackgroundColor(Color.BLACK);
+        startButton.setBackgroundColor(Color.DKGRAY);
         startButton.setTextColor(Color.WHITE);
-        addView(startButton);
+        startButton.setOnClickListener(this::startGame);
+        container.addView(startButton);
 
         // adjust size and positioning of button
         startButton.setLayoutParams(new LinearLayout.LayoutParams(MainActivity.width/2, 300));
@@ -42,5 +49,13 @@ public class MainMenu extends LinearLayout {
         linearLayoutParams.gravity = Gravity.CENTER;
         startButton.setLayoutParams(linearLayoutParams);
 
+        // adjust size and positioning of container
+        LinearLayout.LayoutParams containerLayoutParams = (LinearLayout.LayoutParams)container.getLayoutParams();
+        containerLayoutParams.gravity = Gravity.CENTER;
+        container.setLayoutParams(containerLayoutParams);
+    }
+
+    private void startGame(View view) {
+        System.out.println("Game Starts");
     }
 }
