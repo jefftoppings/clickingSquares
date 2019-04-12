466d6a0d0b785ba8762751a5fd5f50e1d826dd9e
Jeffrey Toppings <jtt607@mail.usask.ca>
2019-04-12 14:47:36 -0600

    Added GameCanvas class to handle drawing circles

---
+++ app/src/main/java/com/example/clickingsquares/GameCanvas.java
@@ -0,0 +1,12 @@
+package com.example.clickingsquares;
+
+import android.content.Context;
+import android.view.View;
+
+class GameCanvas extends View {
+
+
+    public GameCanvas(Context context) {
+        super(context);
+    }
+}
--- app/src/main/java/com/example/clickingsquares/Gameplay.java
+++ app/src/main/java/com/example/clickingsquares/Gameplay.java
@@ -3,8 +3,11 @@ package com.example.clickingsquares;
 import android.content.Context;
 import android.graphics.Color;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Gameplay extends LinearLayout {
+
+    TextView score, timeRemaining;
 
     public Gameplay(Context context) {
         super(context);
@@ -13,6 +16,29 @@ public class Gameplay extends LinearLayout {
         setLayoutParams(new LayoutParams(MainActivity.width, MainActivity.height));
 
         // top section
+        LinearLayout topSection = new LinearLayout(context);
+        addView(topSection);
+        topSection.setLayoutParams(new LayoutParams(MainActivity.width, MainActivity.height/10));
 
+        // Score
+        score = new TextView(context);
+        topSection.addView(score);
+        score.setText("Score: 0");
+        score.setTextColor(Color.WHITE);
+        score.setTextSize(20);
+        score.setLayoutParams(new LayoutParams(MainActivity.width/2, MainActivity.height/10));
+
+        // Time Remaining
+        timeRemaining = new TextView(context);
+        topSection.addView(timeRemaining);
+        timeRemaining.setText("Time Remaining: 60");
+        timeRemaining.setTextColor(Color.WHITE);
+        timeRemaining.setTextSize(20);
+        timeRemaining.setLayoutParams(new LayoutParams(MainActivity.width/2, MainActivity.height/10));
+
+        // Canvas
+        GameCanvas gameCanvas = new GameCanvas(context);
+        addView(gameCanvas);
+        gameCanvas.setLayoutParams(new LayoutParams(MainActivity.width, (int) (MainActivity.height*0.9)));
     }
 }
